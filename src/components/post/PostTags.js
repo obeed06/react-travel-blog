41432bfb957b1d0790b2d746c2cd8461f340f23d
Dave <obeed06@gmail.com>
2022-03-14 10:45:40 +0000

    post tags, move to stack. much less painful

--- src/components/post/PostTags.js
+++ src/components/post/PostTags.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Divider from "@mui/material/Divider";
-import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 
 export default function PostTags({post, tagSize = "x-small"}) {
     let categories = post?.categories
@@ -12,45 +12,32 @@ export default function PostTags({post, tagSize = "x-small"}) {
             countryNames = countryNames.slice(0, 1)
     }
     return (
-        <Box className="postCardTags"
-             sx={{
-                 display: 'flex',
-                 '& hr': {
-                     mx: 0.5,
-                     my: 0.35,
-                     borderColor: "rgba(255, 255, 255, 0.5)"
-                 },
-             }}
-        >
+        <Stack direction={{ xs: 'column', sm: 'row' }}
+               divider={<Divider style={{marginLeft: "5px", marginRight: "5px"}} orientation="vertical" flexItem/>}
+               sx={{alignItems: "center"}}
+               className="postCardTags">
             {categories && categories.map((category, i) =>
-                    (category ? (
-                        <React.Fragment>
-                            <span key={"tag-category-"+category.title+i}
-                                style={category?.colourHex ? {color: category?.colourHex, fontSize: tagSize} : {fontSize: tagSize}}>
+                (category ? (
+                    <span key={"tag-category-" + category.title + i}
+                          style={category?.colourHex ? {
+                              color: category?.colourHex,
+                              fontSize: tagSize
+                          } : {fontSize: tagSize}}>
                                 {category?.title}
-                            </span>
-                            {i === categories.length-1 ? "" : divider("tag-category-divider"+category.title+i)}
-                        </React.Fragment>
-                    ) : (
-                        ""
-                    ))
+                    </span>
+                ) : (
+                    ""
+                ))
             )}
-            {categories && categories.length && countryNames && countryNames.length ? divider("tag-divider") : ""}
             {countryNames && countryNames.map((countryName, i) =>
-                    (countryName ? (
-                        <React.Fragment>
-                            <span style={{fontSize: tagSize}}>
+                (countryName ? (
+                    <span key={"tag-country-" + countryName.title + i} style={{fontSize: tagSize}}>
                                 {countryName}
                             </span>
-                            {i === countryNames.length-1 ? "" : divider("tag-country-divider"+countryName+i)}
-                        </React.Fragment>
-                    ) : (
-                        ""
-                    ))
+                ) : (
+                    ""
+                ))
             )}
-        </Box>
+        </Stack>
     );
-}
-function divider(key) {
-    return <Divider key={Math.random()} orientation="vertical" variant="inset" flexItem/>
 }
