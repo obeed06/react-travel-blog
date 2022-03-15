272944783b7f8dd3ab5fe21e61b8ab54695d85f1
Dave <obeed06@gmail.com>
2022-03-15 19:51:27 +0000

    remove unused var pt 2 always miss one

--- src/components/trip/Trips.js
+++ src/components/trip/Trips.js
@@ -1,7 +1,6 @@
 import './Trips.css';
 import React, {useState, useEffect} from 'react';
 import TripCard from "./TripCard";
-import useWindowPosition from '../../hook/useWindowPosition';
 import Stack from "@mui/material/Stack";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
