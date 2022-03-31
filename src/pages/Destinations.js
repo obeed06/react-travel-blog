f9bb190afdec1342231654e8fea494b7b37045a7
Dave <obeed06@gmail.com>
2022-03-31 18:05:15 +0100

    improve skeleton structure for destinations page

--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -7,12 +7,12 @@ import DestinationGrid from "../components/destination/DestinationGrid";
 import {getDestinations} from "../lib/destinationApi";
 import MapChart from "../components/mapChart/MapChart";
 import Grid from "@mui/material/Grid";
-import Skeleton from "@mui/material/Skeleton";
 import HeaderAndFooter from "../components/HeaderAndFooter";
 import SearchIcon from '@mui/icons-material/Search';
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputAdornment from "@mui/material/InputAdornment";
+import SkeletonHeroPostCard from "../components/post/SkeletonHeroPostCard";
 
 const Destinations = ({preview = false}) => {
     const [destinations, setDestinations] = useState(null);
@@ -23,48 +23,47 @@ const Destinations = ({preview = false}) => {
 
     useEffect(() => {
         getDestinations(preview)
-            .then((data) => { setDestinations(data); setDestinationNames(data.map((d, i) => d.name)); })
+            .then((data) => {
+                setDestinations(data);
+                setDestinationNames(data.map((d, i) => d.name));
+            })
             .catch(console.error);
     }, [preview]);
 
     return <HeaderAndFooter>
         {
-            typeof (destinations) !== 'undefined' && destinations !== null && Array.isArray(destinations) ? (
-                <Box id="destinations" className="section" sx={{py: 5}}>
-                    <Container maxWidth='lg'>
-                        <Box sx={{pt:5}}>
-                            <MapChart visitedGeos={destinationNames} />
-                        </Box>
-                        <Grid container direction="row" justifyContent="center" alignItems="center" >
-                            <Grid item xs={12} md={8}>
-                                <FormControl fullWidth sx={{ m: 1 }}>
-                                    <OutlinedInput
-                                        value={q}
-                                        onChange={(e) => setQ(e.target.value)}
-                                        endAdornment={<InputAdornment position="start"><SearchIcon/></InputAdornment>}
-                                        placeholder="Search for a destination"
-                                    />
-                                </FormControl>
-                            </Grid>
-                        </Grid>
-                        <Parallax translateY={['0', '+48']}>
-                            <Typography vairant="h1" component="h2" className="sectionHeader">
-                                Destinations.
-                            </Typography>
-                        </Parallax>
-                    </Container>
-                    <DestinationGrid destinations={destinations && searchDestinations(destinations, searchParam, q)}/>
-                </Box>
-            )  : (
-                <Box>
-                    <Box >
-                        <Grid sx={{height: "100%"}} container direction="row" justifyContent="center" alignItems="center">
-                            <Skeleton height={80} width={"40%"}/>
-                        </Grid>
-                    </Box>
-                </Box>)
-        }
+            <Box id="destinations" className="section" sx={{py: 5}}>
+                <Container maxWidth='lg'>
+                    <Box sx={{pt: 5}}>
+                        {
+                            destinationNames ?
+                                <MapChart visitedGeos={destinationNames}/>
+                                :
+                                <SkeletonHeroPostCard/>
 
+                        }
+                    </Box>
+                    <Grid container direction="row" justifyContent="center" alignItems="center">
+                        <Grid item xs={12} md={8}>
+                            <FormControl fullWidth sx={{m: 1}}>
+                                <OutlinedInput
+                                    value={q}
+                                    onChange={(e) => setQ(e.target.value)}
+                                    endAdornment={<InputAdornment position="start"><SearchIcon/></InputAdornment>}
+                                    placeholder="Search for a destination"
+                                />
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                    <Parallax translateY={['0', '+48']}>
+                        <Typography vairant="h1" component="h2" className="sectionHeader">
+                            Destinations.
+                        </Typography>
+                    </Parallax>
+                </Container>
+                <DestinationGrid destinations={destinations && searchDestinations(destinations, searchParam, q)}/>
+            </Box>
+        }
     </HeaderAndFooter>
 };
 
